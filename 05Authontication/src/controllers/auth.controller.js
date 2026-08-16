9a92eca150ba6237a640305a7f82c49e44fe52bb
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-08-16 18:38:04 +0530

    laerning new cncept authoncation and authorization

---
+++ 05Authontication/src/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+async function registerUser(req, res) {
+
+    const { username, email, password } = req.body;
+
+    const user = await userModel.create({
+        userName: username,
+        email: email,
+        password: password
+    });
+
+    const token = jwt.sign(
+        {
+            id: user._id
+        },
+        process.env.JWT_SECRET
+    );
+
+    res.json({
+        message: "User registered successfully",
+        user: user,
+        token: token
+    });
+}
+
+module.exports = { registerUser };
---
+++ 05Authontication/src/routes/auth.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const authController = require("../controllers/auth.controller")
+
+
+const router = express.Router();
+
+
+router.post("/register",authController.registerUser (req, res))
+
+module.exports = router;
